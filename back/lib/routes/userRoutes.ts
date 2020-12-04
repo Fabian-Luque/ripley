1cea79a3cc274c315ad0d148511bc5c3c6b5a4c2
Fabian-Luque <Fabianluque2014@gmail.com>
2020-12-04 11:31:10 -0300

    FIX: importacion

--- back/lib/routes/userRoutes.ts
+++ back/lib/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 
-import { AuthController } from "controllers/authController";
+import { AuthController } from "../controllers/authController";
 import { Router } from "express";
 import { UserController } from "../controllers/userController";
 
